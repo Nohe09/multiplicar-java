bba840643abd4204392b91e2bde533fbf6fe80c7
samuelalejo18 <samuelalejomonsalve@gmail.com>
2024-08-01 10:26:54 -0500

    Se adiciono a la clase multiplicar números

---
+++ MultiplicarNumero/src/main/java/com/mycompany/multiplicarnumero/MultiplicarNumero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multiplicarnumero;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MultiplicarNumero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
