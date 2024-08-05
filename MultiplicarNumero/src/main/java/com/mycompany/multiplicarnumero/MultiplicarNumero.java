0a78fc52a9a1f205fc62358bad1a8808ac63c0e9
sn <sn@konradlorenz.edu.co>
2024-08-05 09:56:08 -0500

    se agrego la función de multiplicar

--- MultiplicarNumero/src/main/java/com/mycompany/multiplicarnumero/MultiplicarNumero.java
+++ MultiplicarNumero/src/main/java/com/mycompany/multiplicarnumero/MultiplicarNumero.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.multiplicarnumero;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario
@@ -11,6 +13,14 @@ package com.mycompany.multiplicarnumero;
 public class MultiplicarNumero {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+       
+        int numero1, numero2,r;
+        System.out.println("Ingrese el primer valor");
+        numero1=sc.nextInt();
+        System.out.println("Ingrese el segundo valor");
+        numero2=sc.nextInt();
+        System.out.print("la multiplicacion es:"+numero1*numero2);
+        
     }
 }
